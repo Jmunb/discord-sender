6a40a0699a1dbe214904c5f52b3c95246d4c8fea
jmunb <rubartem@gmail.com>
2022-04-20 18:00:50 +0300

    Add node.js wrapper

---
+++ index.js
@@ -0,0 +1,36 @@
+require('dotenv').config()
+const { exec } = require('child_process');
+const sleep = require('system-sleep');
+const accounts = require("./accounts.json");
+const wallets = require("./wallets.json");
+
+
+function SendMessage (proxy, token, wallet) {
+    const command = `HTTPS_PROXY=https_proxy=${proxy} docker run --net=host -e DISCORD_TOKEN='${token}' -e 'DISCORD_CHANNEL_ID'='${process.env.DISCORD_CHANNEL_ID}' -e 'DISCORD_MESSAGE'='!request ${wallet}' -it dsender`
+    console.log(`!request ${wallet}`)
+    exec(command)
+}
+
+function MakeRequestFromAllWallets (list) {
+    list.forEach((item, index) => {
+        const wallet = wallets[index]
+
+        SendMessage(item.discord_proxy, item.discord_token, wallet.umee)
+        sleep(65 * 1000)
+
+        SendMessage(item.discord_proxy, item.discord_token, wallet.cosmos)
+        sleep(65 * 1000)
+
+        SendMessage(item.discord_proxy, item.discord_token, wallet.juno)
+        sleep(65 * 1000)
+
+        SendMessage(item.discord_proxy, item.discord_token, wallet.osmo)
+        sleep(1000)
+    })
+    sleep(12 * 60 * 60 * 1000 + 6000)
+
+    MakeRequestFromAllWallets(list)
+}
+
+
+MakeRequestFromAllWallets(accounts)
