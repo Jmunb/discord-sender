f8b76eb06fc4958e2b138c55514f468add90977c
jmunb <rubartem@gmail.com>
2022-04-21 21:42:29 +0300

    Make requests by networks

--- index.js
+++ index.js
@@ -3,37 +3,55 @@ const { exec } = require('child_process');
 const sleep = require('system-sleep');
 const accounts = require("./accounts.json");
 const wallets = require("./wallets.json");
+const commands = {
+    umee: [],
+    cosmos: [],
+    juno: [],
+    osmo: []
+}
 
+wallets.slice(0, accounts.length).forEach((item, index) => {
+    const account = accounts[index]
 
-function SendMessage (proxy, token, wallet) {
-    const command = `HTTPS_PROXY=https_proxy=${proxy} docker run --net=host -e DISCORD_TOKEN='${token}' -e 'DISCORD_CHANNEL_ID'='${process.env.DISCORD_CHANNEL_ID}' -e 'DISCORD_MESSAGE'='!request ${wallet}' -i dsender`
-    console.log(`!request ${wallet}`)
-    console.log(command)
-    exec(command, (error, stdout, stderr) => {
-        console.log(error, stdout, stderr)
+    const umeeCommand = MakeCommands(account.discord_proxy, account.discord_token, item.umee)
+    const cosmosCommand = MakeCommands(account.discord_proxy, account.discord_token, item.cosmos)
+    const junoCommand = MakeCommands(account.discord_proxy, account.discord_token, item.juno)
+    const osmoCommand = MakeCommands(account.discord_proxy, account.discord_token, item.osmo)
+
+    commands.umee.push(umeeCommand)
+    commands.cosmos.push(cosmosCommand)
+    commands.juno.push(junoCommand)
+    commands.osmo.push(osmoCommand)
+})
+
+function MakeCommands (proxy, token, wallet) {
+    return `HTTPS_PROXY=https_proxy=${proxy} docker run --net=host -e DISCORD_TOKEN='${token}' -e 'DISCORD_CHANNEL_ID'='${process.env.DISCORD_CHANNEL_ID}' -e 'DISCORD_MESSAGE'='!request ${wallet}' -i dsender`
+}
+
+function makeRequestByNetwork (network) {
+    commands[network].forEach(command => {
+        console.log(`${(new Date)} ${command}`)
+        exec(command, (error, stdout, stderr) => {
+            console.log(error, stdout, stderr)
+        })
+        sleep( 1000)
     })
 }
 
-function MakeRequestFromAllWallets (list) {
-    list.forEach((item, index) => {
-        const wallet = wallets[index]
+function SendRequests () {
+    makeRequestByNetwork('cosmos')
+    sleep(60 * 1000)
 
-        SendMessage(item.discord_proxy, item.discord_token, wallet.umee)
-        sleep(65 * 1000)
+    makeRequestByNetwork('umee')
+    sleep(60 * 1000)
 
-        SendMessage(item.discord_proxy, item.discord_token, wallet.cosmos)
-        sleep(65 * 1000)
+    makeRequestByNetwork('juno')
+    sleep(60 * 1000)
 
-        SendMessage(item.discord_proxy, item.discord_token, wallet.juno)
-        sleep(65 * 1000)
+    makeRequestByNetwork('osmo')
+    sleep(Number(process.env.SNOOZE_TIME) * 60 * 60 * 1000 + 60000)
 
-        SendMessage(item.discord_proxy, item.discord_token, wallet.osmo)
-        sleep(1000)
-    })
-    sleep(Number(process.env.SNOOZE_TIME) * 60 * 60 * 1000 + 6000)
-
-    MakeRequestFromAllWallets(list)
+    SendRequests()
 }
 
-
-MakeRequestFromAllWallets(accounts)
+SendRequests()
